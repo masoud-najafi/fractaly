3d2c92c1e061b85a978e96de66cb3167ec04f012
masoud_altair <masoud@altair.com>
2025-04-08 22:10:53 +0200

    export only one symbol for Linux and Windows

--- src/fractaly/core/fractal_maths.c
+++ src/fractaly/core/fractal_maths.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-__declspec(dllexport) int adder(int a, int b) {
+
+#if defined _WIN32 || defined __CYGWIN__
+/* Note: both gcc & MSVC on Windows support this syntax. */
+#define MY_Export __declspec(dllexport)
+#else
+#if __GNUC__ >= 4
+#define MY_Export __attribute__ ((visibility ("default")))
+#else
+#define MY_Export
+#endif
+#endif
+
+
+MY_Export int adder(int a, int b) {  // only adder is exported for Win64 and Linux64
     return a+b;
 }
  
-__declspec(dllexport) int product(int a, int b) {
+ int product(int a, int b) {
     return a *b;
 }
 
 // cl /LD /Fefractal_maths.dll fractal_maths.c /link /EXPORT:adder
-// gcc -shared -o fractal_maths.dll fractal_maths.c -Wl,--export-all-symbols
+// gcc -fPIC -shared -o libfractal_maths.so fractal_maths.c  -fvisibility=hidden
